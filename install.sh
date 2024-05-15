caddad9168c6041be92c5fa0d75f0be3425d7955
Kev <36553759+forkymcforkface@users.noreply.github.com>
2024-05-15 13:28:57 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ sudo sh -c "echo 'i2c-dev' >> /etc/modules"
 ## This is for autoboot driver but Pi hangs
 ##sudo sh -c "echo 'joypi' >> /etc/modules"
 cd joypi
-sudo make clean
 sudo make
 sudo make install
 sudo insmod joypi.ko
