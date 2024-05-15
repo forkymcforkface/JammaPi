298713d49d24a3e72f7f4660110f70b25880f7c5
Kev <36553759+forkymcforkface@users.noreply.github.com>
2024-05-15 09:39:06 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,5 @@ sudo sh -c "echo 'i2c-dev' >> /etc/modules"
 ##sudo sh -c "echo 'joypi' >> /etc/modules"
 cd joypi
 make clean
-cd src
-# The following 2 lines are due to GCC greater than 5.0.0
-rm mcp23017.c
-mv mcp23017-5.c mcp23017.c
-cd ~/dev/JammaPi/joypi/
-make
 sudo make install
 sudo insmod joypi.ko
-cd ..
-cd ..
